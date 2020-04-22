105767f7501d47be3aab2cb88f5e430490fa4dbc
Dmitry Shumeev <diman1shumeev@yandex.ru>
2020-04-22 20:00:26 +0300

    init

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='imgorderer',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='',
+      author='dinosf',
+      author_email='dinosf@mail.com',
+      license='MIT',
+      packages=['imgorderer', 'nextgen'],
+      zip_safe=False)
---
+++ test/nextgen/nexxer.py
@@ -0,0 +1,8 @@
+import unittest
+
+from ..nextgen.nexxer import Nexxer
+
+def nexxer_factory():
+    return Nexxer()
+class NexxerTest(unittest.TestCase):    
+    
